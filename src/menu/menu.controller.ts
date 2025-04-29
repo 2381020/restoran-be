5e8876b7df09fd6ebe9778fc885590171800f17c
Andrew Simbolon <2381020@unai.edu>
2025-04-29 16:42:27 +0700

    minor bug fix

--- src/menu/menu.controller.ts
+++ src/menu/menu.controller.ts
@@ -1,18 +1,30 @@
-import { Controller, Post, Body, Get } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Get,
+  UseGuards,
+  Req,
+} from '@nestjs/common';
 import { MenuService } from './menu.service';
-import { CreateMenuDto } from './create-menu-dto'; // pastikan path-nya benar
+import { CreateMenuDto } from './create-menu-dto';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('menus')
 export class MenuController {
   constructor(private readonly menuService: MenuService) {}
 
+  @UseGuards(AuthGuard('jwt')) // Lindungi hanya endpoint POST
   @Post()
-  async createMenu(@Body() createMenuDto: CreateMenuDto) {
+  async createMenu(
+    @Body() createMenuDto: CreateMenuDto,
+    @Req() req
+  ) {
     return this.menuService.create(createMenuDto);
   }
 
   @Get()
-  async getAllMenus() {
-    return this.menuService.findAll(); // pastikan method ini ada di MenuService
+  async getAllMenus(): Promise<any[]> {
+    return this.menuService.findAll();
   }
 }
